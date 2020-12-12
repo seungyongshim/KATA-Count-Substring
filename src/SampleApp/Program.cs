9a563d4cb53d6bd8994c5ea27dc106984590355e
심승용 <seungyong.shim@mirero.co.kr>
2020-12-12 13:25:43 +0900

    완성

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Linq;
+using FluentAssertions;
+
 
 namespace SampleApp
 {
@@ -6,7 +9,43 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ret = CountSubString("ababa", "aba");
+
+            ret.Should().Be(5);
+        }
+
+        private static int CountSubString(string ori, string sub)
+        {
+            var count = 0;
+
+            SubMatch(0, 0);
+
+            return count;
+
+
+            void SubMatch(int oriPos, int subPos)
+            {
+                if (subPos >= sub.Length)
+                {
+                    count++;
+                    return;
+                }
+
+                if (oriPos >= ori.Length) return ;
+                
+
+                if(ori[oriPos] == sub[subPos])
+                {
+                    for (int i = oriPos + 1; i < ori.Length + 1; i++)
+                    {
+                        SubMatch(i, subPos + 1);
+                    }
+                }
+                return ;
+            }
+
+
+            return 0;
         }
     }
 }
