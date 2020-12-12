2b3161f51512b75b4e239dae8896ac90386f5e3c
심승용 <seungyong.shim@mirero.co.kr>
2020-12-12 18:43:51 +0900

    피보니ㅊ

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Numerics;
+using FluentAssertions;
 
 namespace SampleApp
 {
     class Program
     {
+        static BigInteger fibonacciModified(int t1, int t2, int n)
+        {
+            var t = new BigInteger[n];
+
+            t[0] = new BigInteger(t1);
+            t[1] = new BigInteger(t2);
+
+            for (int i = 2; i < n; i++)
+            {
+                t[i] = t[i - 2] + BigInteger.Pow(t[i - 1], 2);
+            }
+
+            return t[n - 1];
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ret = fibonacciModified(1, 1, 20);
         }
     }
 }
