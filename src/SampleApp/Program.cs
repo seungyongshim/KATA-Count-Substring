e9b2d380268a8d6e1f4748675e644e4123f2dbb1
심승용 <seungyong.shim@mirero.co.kr>
2020-12-12 20:12:03 +0900

    stockmax

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
 
 namespace SampleApp
 {
     class Program
     {
+        static List<int> Prices;
+
+        public static long stockmax(List<int> prices)
+        {
+            Prices = prices;
+
+            var ret = BuyOfsell(0, 0, 0);
+
+            return ret;
+        }
+
+        static long BuyOfsell(int stocks, int day, long money)
+        {
+            if (day >= Prices.Count)
+            {
+                return money;
+            }
+            // 1주 사거나
+            var a = BuyOfsell(stocks + 1, day + 1, money - Prices[day]);
+
+            // 팔거나
+            var b = BuyOfsell(0, day + 1, money + Prices[day] * stocks);
+
+            // 거래하지 않거나
+            var c = BuyOfsell(stocks , day + 1, money - Prices[day]);
+
+            return Math.Max(a, Math.Max(b, c));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            stockmax(new List<int> { 1, 2, 100 }).Should().Be(197);
         }
     }
 }
